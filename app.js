d4258787a586dd11b2f1398a2335101d887e2fe6
imkarthi16 <karthiveera@gmail.com>
2022-08-23 10:58:02 +0530

    Fetching data

--- app.js
+++ app.js
@@ -17,5 +17,15 @@ connectToDb((err) => {
 
 // routes
 app.get("/books", (req, res) => {
+  let books = [];
+  db.collection()
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book));
+  then(() => {
+    res.status(200).json(books);
+  }).catch(() => {
+    res.status(500).json({ error: "Could not fetch the documents" });
+  });
   res.json({ mssg: "welcome to the api" });
 });
