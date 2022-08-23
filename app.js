5450ba9b239aaae765ee62ced4619d1b39998097
imkarthi16 <karthiveera@gmail.com>
2022-08-23 12:11:09 +0530

    update patch request

--- app.js
+++ app.js
@@ -1,60 +1,104 @@
-const { ObjectId } = require("bson");
-const express = require("express");
-const { getDb, connectToDb } = require("./db");
+const express = require('express')
+const { getDb, connectToDb } = require('./db')
+const { ObjectId } = require('mongodb')
 
 // init app & middleware
-const app = express();
+const app = express()
+app.use(express.json())
 
 // db connection
-let db;
+let db
 
 connectToDb((err) => {
-  if (!err) {
-    app.listen("3000", () => {
-      console.log("app listening on port 3000");
-    });
-    db = getDb();
+  if(!err){
+    app.listen('3000', () => {
+      console.log('app listening on port 3000')
+    })
+    db = getDb()
   }
-});
+})
 
 // routes
-app.get("/books", (req, res) => {
-  let books = [];
+app.get('/books', (req, res) => {
+  let books = []
 
-  db.collection("books")
+  db.collection('books')
     .find()
-    .sort({ author: 1 })
-    .forEach((book) => books.push(book))
+    .sort({author: 1})
+    .forEach(book => books.push(book))
     .then(() => {
-      res.status(200).json(books);
+      res.status(200).json(books)
     })
     .catch(() => {
-      res.status(500).json({ error: "Could not fetch the documents" });
-    });
-});
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
+})
 
-app.get("/books/:id", (req, res) => {
+app.get('/books/:id', (req, res) => {
+
   if (ObjectId.isValid(req.params.id)) {
-    db.collection("books")
-      .findOne({ _id: ObjectId(req.params.id) })
-      .then((doc) => {
-        res.status(200).json(doc);
-      })
-      .catch(() => {
-        res.status(500).json({ error: "Could not fetch the document" });
-      });
-  }
-});
 
-app.delete("/books/:id",(req,res)=>{
-    if (ObjectId.isValid(req.params.id)) {
-        db.collection("books")
-          .deleteOne({ _id: ObjectId(req.params.id) })
-          .then((result) => {
-            res.status(200).json(result);
-          })
-          .catch(() => {
-            res.status(500).json({ error: "Could not delete the document" });
-          });
-      }
+    db.collection('books')
+      .findOne({_id: new ObjectId(req.params.id)})
+      .then(doc => {
+        res.status(200).json(doc)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not fetch the document'})
+      })
+      
+  } else {
+    res.status(500).json({error: 'Could not fetch the document'})
+  }
+
+})
+
+app.post('/books', (req, res) => {
+  const book = req.body
+
+  db.collection('books')
+    .insertOne(book)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({err: 'Could not create new document'})
+    })
+})
+
+app.delete('/books/:id', (req, res) => {
+
+  if (ObjectId.isValid(req.params.id)) {
+
+  db.collection('books')
+    .deleteOne({ _id: new ObjectId(req.params.id) })
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({error: 'Could not delete document'})
+    })
+
+  } else {
+    res.status(500).json({error: 'Could not delete document'})
+  }
+})
+
+app.patch('/books/:id', (req, res) => {
+  const updates = req.body
+
+  if (ObjectId.isValid(req.params.id)) {
+
+    db.collection('books')
+      .updateOne({ _id: new ObjectId(req.params.id) }, {$set: updates})
+      .then(result => {
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json({error: 'Could not update document'})
+      })
+
+  } else {
+    res.status(500).json({error: 'Could not update document'})
+  }
 })
