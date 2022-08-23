d10d1266c7914c118315d470a071eed98ebd8d1d
imkarthi16 <karthiveera@gmail.com>
2022-08-23 10:23:48 +0530

    Creating node js project & install MongoDb Drivers

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+// init app & middleware
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("app listining on port 3000");
+});
+
+// routes
+app.get("/books", (req, res) => {
+  res.json({ mssg: "welcome to the api" });
+});
